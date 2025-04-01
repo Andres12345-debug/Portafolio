d7239bdf70697047ad65a91b9739d6e3b5ba11c9
Andres527 <julian2000mk1@gmail.com>
2025-04-01 05:30:30 -0500

    Se sube el proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { ToastrModule } from 'ngx-toastr';
+import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './components/contenedor/inicio/inicio.component';
+import { ErrorComponent } from './components/contenedor/error/error.component';
+import { FooterComponent } from './components/contenedor/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    ErrorComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ToastrModule.forRoot(), // ToastrModule added
+    // ToastrModule.forRoot() // ToastrModule added
+    BsDatepickerModule.forRoot(),
+    ModalModule,
+
+
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
