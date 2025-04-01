56942324ba2eaad4cf8db92c12a70331d154ffd8
Andres527 <julian2000mk1@gmail.com>
2025-04-01 06:23:15 -0500

    Se agrega foto y fondos para el portafolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './components/contenedor/inicio/inicio.component';
+import { ErrorComponent } from './components/contenedor/error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component: InicioComponent},
+
+  /* RUTAS OBLIGATORIAS*/
+  {path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  {path: '**', component: ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
